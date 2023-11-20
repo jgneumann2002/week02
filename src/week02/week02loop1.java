f37f9d2784bf9823b1e1ec07a49f51cfb3ed3f89
jgneumann2002 <151477139+jgneumann2002@users.noreply.github.com>
2023-11-19 18:09:14 -0600

    Initial commit

---
+++ src/week02/week02loop1.java
@@ -0,0 +1,43 @@
+package week02;
+
+
+
+
+public class week02loop1 {
+
+	public static void main(String[] args) {
+		
+		
+		// Iterate through numbers from 1 to 99
+        for (int i = 1; i < 100; i++) {
+            // Check if the number is odd
+            if (!(i % 2 == 0)) {
+                // Print the odd number
+                System.out.println(i);
+		}
+            
+            
+        }   
+	   
+                 
+	}	       
+          
+	}	            
+    	
+                	
+    	
+    	
+    	
+    	
+          	
+
+          
+	
+
+
+           
+            
+        
+	
+	
+
